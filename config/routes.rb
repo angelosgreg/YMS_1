739a3034f7e8eca38844727916768c5d6af7089b
Angelos Grigorakis <angelosgreg13@gmail.com>
2023-10-10 19:03:13 +0200

    last draft

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :yachts do
-    # resources :bookings, only: [:create, :new]
-  end
-end
